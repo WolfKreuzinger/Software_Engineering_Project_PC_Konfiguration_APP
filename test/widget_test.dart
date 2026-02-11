6436b0656fc25f717725ca1a9ad9608d49d00ec5
WolfKreuzinger <wek.kreuzinger@gmail.com>
2026-02-11 22:05:52 +0100

    changed widget_test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,9 +11,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:software_engineering_project_pc_konfiguration_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('app starts (smoke test)', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const BuildMyPCApp());
+    await tester.pumpAndSettle();
+
+    expect(find.byType(BuildMyPCApp), findsOneWidget);
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
