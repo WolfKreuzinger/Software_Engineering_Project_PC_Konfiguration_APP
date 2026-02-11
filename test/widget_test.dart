3562d7e90c868991b9d45799ce0f7832830d5e5e
WolfKreuzinger <wek.kreuzinger@gmail.com>
2026-02-11 21:51:04 +0100

    changed name of App in test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:software_engineering_project_pc_konfiguration_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const BuildMyPCApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
